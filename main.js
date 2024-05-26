5ead7723abe3f94019ff6faeba14f320d4f25d86
n000wing <nikitavasiliev0022@gmail.com>
2024-05-26 17:33:17 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+  var swiper = new Swiper(".mySwiper", {
+      effect: "fade",
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+      },
+    });
